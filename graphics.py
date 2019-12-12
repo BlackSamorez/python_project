c137c281a1a789b2943e2c06759e00ff5f03a6fe
SumarenkoED <Sumarenko.ED@phystech.edu>
2019-12-12 22:05:19 +0300

    entity, menu

--- graphics.py
+++ graphics.py
@@ -1,51 +1,64 @@
 from math import *
 import time
-from random import randrange as rnd,choice
 import tkinter as tk
 import os
-import cv2
 
 root = tk.Tk()
 fr = tk.Frame(root)
 root.geometry('1920x1080')
-canv = tk.Canvas(root,bg='grey')
-canv.pack(fill = tk.BOTH,expand = 1)
+canv = tk.Canvas(root, bg='grey')
+canv.pack(fill=tk.BOTH, expand=1)
 
 
-def _from_rgb(rgb): #HEX from rgb
-    return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])   
+def _from_rgb(rgb):  # HEX from rgb
+	return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
 
-class Vector2D: #vectors
+
+class Vector2D:  # vectors
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
+
 	def __add__(self, o):
 		return Vector2D(self.x + o.x, self.y + o.y)
+
 	def __sub__(self, o):
 		return Vector2D(self.x - o.x, self.y - o.y)
+
 	def __truediv__(self, a):
 		return Vector2D(self.x / a, self.y / a)
+
 	def __mul__(self, o):
 		if isinstance(o, Vector2D):
 			return self.x * o.x + self.y * o.y
 		else:
 			return Vector2D(self.x * o, self.y * o)
+
 	def __mod__(self, o):
 		return self.x * o.y - self.y * o.x
+
 	def __str__(self):
 		return '(' + str(self.x) + ', ' + str(self.y) + ')'
 
-#Vector operations
+
+# Vector operations
 def v_abs2(x):
 	return x * x
+
+
 def v_abs(x):
 	return sqrt(x * x)
+
+
 def v_norm(x):
 	return x / v_abs(x)
+
+
 def v_rot(x):
 	return Vector2D(-x.y, x.x)
 
-#what we control
+
+# what we control
 class Player:
 	def __init__(self, look, position):
 		self.look = look
@@ -54,15 +67,22 @@ class Player:
 		self.right = 0
 		self.rot = 0
 
-#not used yet
-class Entity:
-	def __init__(self, look, position):
-		self.look = look
-		self.position = position
 
-#map object
+# not used yet
+class Entity:
+	def __init__(self, position=Vector2D(0, 0)):
+		self.position = position
+		self.dist = 1
+		self.height = 1
+		self.width = 0.5
+		self.id = -1
+		self.color = [0, 100, 100]
+		self.lh = 1
+
+
+# map object
 class Scene:
-	def __init__(self, filename = None):
+	def __init__(self, filename=None):
 		self.renderwidth = 20
 		self.width = 1920
 		self.height = 1080
@@ -81,28 +101,47 @@ class Scene:
 				lx, ly, x, y = [float(x) for x in args.split(' ')]
 				self.player = Player(v_norm(Vector2D(lx, ly)), Vector2D(x, y))
 		self.lines = 3
-		self.color = [[]*3 for x in range(self.lines)]
+		self.color = [[] * 3 for x in range(self.lines)]
 		self.edges = [0, 0, 0]
 		self.bc = [255, 255, 255]
-		
+		self.entities = []
+		self.entity_trace = [Entity() for x in range(self.width // self.renderwidth)]
 
-	def display(self):
+	def display_entities(self):
+		self.entity_trace = [Entity() for x in range(self.width // self.renderwidth)]
+		for ent in self.entities:
+			phi = atan2((ent.position.y - self.player.position.y), (ent.position.x - self.player.position.x)) - atan2(
+				self.player.look.y, self.player.look.x)
+			if phi < self.camx / 2 and phi > - self.camx:
+				x_ = int((self.camx / 2 + phi) / self.camx * self.width) // self.renderwidth
+				ent.dist = sqrt(
+					(ent.position.x - self.player.position.x) ** 2 + (ent.position.y - self.player.position.y) ** 2)
+				ent.lh = int(ent.height / (ent.dist + 0.0001) / self.camy * self.height / 2)
+				# canv.create_rectangle(x_ * self.renderwidth - self.renderwidth // 2, self.height // 2 - lh , x_ * self.renderwidth + self.renderwidth // 2, self.height // 2 + lh , fill = _from_rgb([int(ent.color[0]), int(ent.color[1]), int(ent.color[2])]))
+				deltaphi = int((atan2(ent.width, ent.dist) / self.camx) * self.width / self.renderwidth) // 2
+				for i in range(2 * deltaphi):
+					if (x_ - deltaphi + i) > -1 and (x_ - deltaphi + i) < (self.width // self.renderwidth):
+						self.entity_trace[x_ - deltaphi + i] = ent
+
+	def display_cubes(self):
 		canv.delete("all")
 
-		canv.create_rectangle(0, self.height // 2, self.width, self.height, fill = 'black')
+		canv.create_rectangle(0, self.height // 2, self.width, self.height, fill='black')
 
-		self.edges = [0,20,80, 100]
+		self.edges = [0, 20, 80, 100]
 		self.color[0] = [87, 31, 0]
 		self.color[1] = [255, 255, 255]
 		self.color[2] = [87, 31, 0]
 		self.bc = [255, 0, 0]
 
 		x_old = -1
-		for x_ in range(self.width // self.renderwidth):
+		for x_ in range(int(self.width // self.renderwidth)):
 			debug = (x_ == self.width // 2)
 
 			player = self.player
-			to = v_norm(v_rot(player.look) * sin(self.camx * x_ * self.renderwidth / self.width - 0.5) + player.look * cos(self.camx * x_ * self.renderwidth / self.width - 0.5))
+			to = v_norm(
+				v_rot(player.look) * sin(self.camx * x_ * self.renderwidth / self.width - 0.5) + player.look * cos(
+					self.camx * x_ * self.renderwidth / self.width - 0.5))
 			ok_walls = []
 			x = player.position
 			deltaDistX = abs(1 / to.x)
@@ -138,7 +177,7 @@ class Scene:
 					hit = self.field[mapX][mapY]
 					if self.field[mapX][mapY] == 0:
 						self.field[mapX][mapY] = 1
-			
+
 			if hit != -1:
 				if side == 0:
 					perpWallDist = (mapX - x.x + (1 - stepX) / 2) / to.x;
@@ -157,26 +196,86 @@ class Scene:
 				wallX = wallX - int(wallX)
 				brightness = brightness * sqrt(abs(cos(x_ * self.renderwidth / (self.width / 2) + pi / 2)))
 
-				if hit in [0, 1]:
-					for i in range(len(self.edges) - 1):
-						canv.create_rectangle(x_ * self.renderwidth - self.renderwidth / 2, self.height // 2 - lh + 2 * lh * (self.edges[i] / 100) , x_ * self.renderwidth + self.renderwidth / 2, self.height / 2 - lh + 2 * lh * (self.edges[i + 1] / 100) , fill = _from_rgb([int(self.color[i][0] * brightness), int(self.color[i][1] * brightness), int(self.color[i][2] * brightness)]))
-				if hit == 2:
-						canv.create_rectangle(x_ * self.renderwidth - self.renderwidth / 2, self.height // 2 - lh / 2, x_ * self.renderwidth + self.renderwidth / 2, self.height / 2 + lh / 2, fill = _from_rgb([int(self.bc[0] * brightness), int(self.bc[1] * brightness), int(self.bc[2] * brightness)]))
-			
+				ent = self.entity_trace[x_]
+				if True:
+					if ent.id != -1:
+						if perpWallDist < ent.dist:
+							canv.create_rectangle(x_ * self.renderwidth - self.renderwidth // 2,
+												  self.height // 2 - ent.lh // 2,
+												  x_ * self.renderwidth + self.renderwidth // 2,
+												  self.height // 2 + ent.lh // 2, fill=_from_rgb(
+									[int(ent.color[0]), int(ent.color[1]), int(ent.color[2])]))
+							if hit in [0, 1]:
+								for i in range(len(self.edges) - 1):
+									canv.create_rectangle(x_ * self.renderwidth - self.renderwidth / 2,
+														  self.height // 2 - lh + 2 * lh * (self.edges[i] / 100),
+														  x_ * self.renderwidth + self.renderwidth / 2,
+														  self.height / 2 - lh + 2 * lh * (self.edges[i + 1] / 100),
+														  fill=_from_rgb([int(self.color[i][0] * brightness),
+																		  int(self.color[i][1] * brightness),
+																		  int(self.color[i][2] * brightness)]))
+							if hit == 2:
+								canv.create_rectangle(x_ * self.renderwidth - self.renderwidth / 2,
+													  self.height // 2 - lh / 2,
+													  x_ * self.renderwidth + self.renderwidth / 2,
+													  self.height / 2 + lh / 2, fill=_from_rgb(
+										[int(self.bc[0] * brightness), int(self.bc[1] * brightness),
+										 int(self.bc[2] * brightness)]))
+						else:
+							if hit in [0, 1]:
+								for i in range(len(self.edges) - 1):
+									canv.create_rectangle(x_ * self.renderwidth - self.renderwidth / 2,
+														  self.height // 2 - lh + 2 * lh * (self.edges[i] / 100),
+														  x_ * self.renderwidth + self.renderwidth / 2,
+														  self.height / 2 - lh + 2 * lh * (self.edges[i + 1] / 100),
+														  fill=_from_rgb([int(self.color[i][0] * brightness),
+																		  int(self.color[i][1] * brightness),
+																		  int(self.color[i][2] * brightness)]))
+							if hit == 2:
+								canv.create_rectangle(x_ * self.renderwidth - self.renderwidth / 2,
+													  self.height // 2 - lh / 2,
+													  x_ * self.renderwidth + self.renderwidth / 2,
+													  self.height / 2 + lh / 2, fill=_from_rgb(
+										[int(self.bc[0] * brightness), int(self.bc[1] * brightness),
+										 int(self.bc[2] * brightness)]))
+							canv.create_rectangle(x_ * self.renderwidth - self.renderwidth // 2,
+												  self.height // 2 - ent.lh // 2,
+												  x_ * self.renderwidth + self.renderwidth // 2,
+												  self.height // 2 + ent.lh // 2, fill=_from_rgb(
+									[int(ent.color[0]), int(ent.color[1]), int(ent.color[2])]))
+					else:
+						if hit in [0, 1]:
+							for i in range(len(self.edges) - 1):
+								canv.create_rectangle(x_ * self.renderwidth - self.renderwidth / 2,
+													  self.height // 2 - lh + 2 * lh * (self.edges[i] / 100),
+													  x_ * self.renderwidth + self.renderwidth / 2,
+													  self.height / 2 - lh + 2 * lh * (self.edges[i + 1] / 100),
+													  fill=_from_rgb([int(self.color[i][0] * brightness),
+																	  int(self.color[i][1] * brightness),
+																	  int(self.color[i][2] * brightness)]))
+						if hit == 2:
+							canv.create_rectangle(x_ * self.renderwidth - self.renderwidth / 2,
+												  self.height // 2 - lh / 2,
+												  x_ * self.renderwidth + self.renderwidth / 2,
+												  self.height / 2 + lh / 2, fill=_from_rgb(
+									[int(self.bc[0] * brightness), int(self.bc[1] * brightness),
+									 int(self.bc[2] * brightness)]))
+
 		canv.update()
 
-#start moving
+
+# start moving
 def move_detect(event):
 	global player, show_minimap
 	if event.char == 'w':
 		s.player.forward = 1
-		
+
 	if event.char == 'a':
 		s.player.right = 1
-		
+
 	if event.char == 's':
 		s.player.forward = -1
-		
+
 	if event.char == 'd':
 		s.player.right = -1
 
@@ -190,19 +289,19 @@ def move_detect(event):
 		show_minimap = 1
 
 
-#stop moving
+# stop moving
 def move_undetect(event):
 	global player, a, show_minimap
 
 	if event.char == 'w':
 		s.player.forward = 0
-		
+
 	if event.char == 'a':
 		s.player.right = 0
-		
+
 	if event.char == 's':
 		s.player.forward = 0
-		
+
 	if event.char == 'd':
 		s.player.right = 0
 
@@ -215,22 +314,25 @@ def move_undetect(event):
 	if event.char == 'm':
 		show_minimap = 0
 
-#move itself
+
+# move itself
 def player_move():
 	global player, a
-	if s.player.forward == 1 and s.field[floor(s.player.position.x + s.player.look.x * 0.3)][floor(s.player.position.y + s.player.look.y * 0.3)] == -1:
+	if s.player.forward == 1 and s.field[floor(s.player.position.x + s.player.look.x * 0.3)][
+		floor(s.player.position.y + s.player.look.y * 0.3)] == -1:
 		s.player.position = s.player.position + s.player.look * 0.2
-	
-	if s.player.forward == -1 and s.field[floor(s.player.position.x - s.player.look.x * 0.3)][floor(s.player.position.y - s.player.look.y * 0.3)] == -1:
+
+	if s.player.forward == -1 and s.field[floor(s.player.position.x - s.player.look.x * 0.3)][
+		floor(s.player.position.y - s.player.look.y * 0.3)] == -1:
 		s.player.position = s.player.position - s.player.look * 0.2
 
-	if s.player.right == 1 and s.field[floor(s.player.position.x - v_rot(s.player.look).x * 0.3)][floor(s.player.position.y - v_rot(s.player.look).y * 0.3)] == -1:
+	if s.player.right == 1 and s.field[floor(s.player.position.x - v_rot(s.player.look).x * 0.3)][
+		floor(s.player.position.y - v_rot(s.player.look).y * 0.3)] == -1:
 		s.player.position = s.player.position - v_rot(s.player.look) * 0.2
-	
-	if s.player.right == -1 and s.field[floor(s.player.position.x + v_rot(s.player.look).x * 0.3)][floor(s.player.position.y + v_rot(s.player.look).y * 0.3)] == -1:
+
+	if s.player.right == -1 and s.field[floor(s.player.position.x + v_rot(s.player.look).x * 0.3)][
+		floor(s.player.position.y + v_rot(s.player.look).y * 0.3)] == -1:
 		s.player.position = s.player.position + v_rot(s.player.look) * 0.2
-
-
 
 	if s.player.rot == 1:
 		a -= 5 / 180 * pi
@@ -240,8 +342,9 @@ def player_move():
 		a += 5 / 180 * pi
 		s.player.look = Vector2D(cos(a), sin(a))
 
+
 class minimap():
-	def __init__(self, scene, scenewidth = 0, sceneheight = 0):
+	def __init__(self, scene, scenewidth=0, sceneheight=0):
 		self.war_mist = 0
 		self.scale = 2
 		self.a = 500
@@ -252,31 +355,41 @@ class minimap():
 		self.dx = self.a / self.n * self.scale
 		self.dy = self.a / self.k * self.scale
 		self.obzor = scene.camy
+
 	def draw(self):
 		for i in range(self.n):
 			for j in range(self.k):
 				if self.war_mist:
 					if self.field[i][j] not in [-1, 0]:
-						canv.create_rectangle(i * self.dx, j * self.dy, (i + 1) * self.dx, (j + 1) * self.dy, fill = 'red')
+						canv.create_rectangle(i * self.dx, j * self.dy, (i + 1) * self.dx, (j + 1) * self.dy,
+											  fill='red')
 					if self.field[i][j] == 2:
-						canv.create_rectangle(i * self.dx, j * self.dy, (i + 1) * self.dx, (j + 1) * self.dy, fill = 'blue')
+						canv.create_rectangle(i * self.dx, j * self.dy, (i + 1) * self.dx, (j + 1) * self.dy,
+											  fill='blue')
 				else:
 					if self.field[i][j] != -1:
-						canv.create_rectangle(i * self.dx, j * self.dy, (i + 1) * self.dx, (j + 1) * self.dy, fill = 'red')
+						canv.create_rectangle(i * self.dx, j * self.dy, (i + 1) * self.dx, (j + 1) * self.dy,
+											  fill='red')
 					if self.field[i][j] == 2:
-						canv.create_rectangle(i * self.dx, j * self.dy, (i + 1) * self.dx, (j + 1) * self.dy, fill = 'blue')
-					
-						
+						canv.create_rectangle(i * self.dx, j * self.dy, (i + 1) * self.dx, (j + 1) * self.dy,
+											  fill='blue')
 
-		canv.create_line(self.player.position.x * self.a * self.scale / self.n, self.player.position.y * self.a * self.scale / self.k, self.player.position.x * self.a * self.scale / self.n + self.player.look.x * 10 * self.scale, self.player.position.y * self.a * self.scale / self.k + self.player.look.y * 10 * self.scale, width = 5, fill = 'blue')
-		canv.update() 
+		canv.create_line(self.player.position.x * self.a * self.scale / self.n,
+						 self.player.position.y * self.a * self.scale / self.k,
+						 self.player.position.x * self.a * self.scale / self.n + self.player.look.x * 10 * self.scale,
+						 self.player.position.y * self.a * self.scale / self.k + self.player.look.y * 10 * self.scale,
+						 width=5, fill='blue')
+		canv.update()
+
+# main body
 
 
-#main body
 if __name__ == "__main__":
-	os.system('python3 ./map_generator/auto_generator.py')
-	s = Scene('scene.cfg')
+	os.system('/map_generator/auto_generator.py')
+	s = Scene('map_generator/scene.cfg')
 	a = atan2(s.player.look.x, s.player.look.y)
+	s.entities += [Entity(s.player.position + Vector2D(1, 1))]
+	s.entities[0].id = 0
 	frame = 0
 	begin = time.time()
 	canv.bind("<KeyPress>", move_detect)
@@ -286,18 +399,18 @@ if __name__ == "__main__":
 	mnmp.draw()
 	time1 = 0
 
-
 	while True:
 		frame += 1
 		if frame % 60 == 0:
-			#print(60 / (time.time() - time1))
+			# print(60 / (time.time() - time1))
 			time1 = time.time()
-		
+
 		if show_minimap:
 			mnmp.draw()
+			time.sleep(0.5)
 		else:
-			s.display()
+			s.display_entities()
+			s.display_cubes()
 			player_move()
 
 	root.mainloop()
-
---
+++ menu.py
@@ -0,0 +1,84 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+import os
+
+root = tk.Tk()
+root.geometry('1920x1080')
+canv = tk.Canvas(root, bg='grey')
+canv.pack(fill=tk.BOTH, expand=1)
+
+def start():
+    os.system('graphics.py')
+
+
+def draw_tutorial():
+    pass
+
+def read_scene(filename='scene.cfg'):
+    with open(filename, 'r') as file:
+        s = file.read()
+    fw, fh = [int(x) for x in s.split('\n')[0].split(' ')]
+    field = [[-1] * fh for x in range(fw)]
+    for line in s.split('\n')[1:]:
+        command, args = line.split(' ', 1)
+        if command == 'brick':
+            x, y, tid = [int(x) for x in args.split(' ')]
+            field[x][y] = tid
+    return field
+
+
+def draw_map():
+    global field
+    scale = 1
+    a = 500
+    n = len(field)
+    k = len(field[0])
+    dx = a / n * scale
+    dy = a / k * scale
+    for i in range(n):
+        for j in range(k):
+            if field[i][j] != -1:
+                canv.create_rectangle(
+                    500 + i * dx, 200 + j * dy,
+                    500 + (i + 1) * dx, 200 + (j + 1) * dy,
+                    fill='black'
+                )
+            if field[i][j] == 2:
+                canv.create_rectangle(
+                    500 + i * dx, 200 + j * dy,
+                    500 + (i + 1) * dx, 200 + (j + 1) * dy,
+                    fill='blue'
+                )
+
+
+global field
+field = read_scene()
+pilImage = Image.open("hell.jpg")
+bg = ImageTk.PhotoImage(pilImage)
+canv.create_image(700, 210, image=bg)
+start_button = tk.Button(
+    root, text='Start',
+    width=15, height=2,
+    bg='grey', fg='black',
+    activeforeground='red', activebackground='grey',
+    command=start
+)
+start_button.place(x=650, y=40)
+map_button = tk.Button(
+    root, text='Reroll map',
+    width=15, height=2,
+    bg='grey', fg='black',
+    activeforeground='red', activebackground='grey',
+    command=draw_map
+)
+map_button.place(x=650, y=90)
+tutorial_button = tk.Button(
+    root, text='Tutorial',
+    width=15, height=2,
+    bg='grey', fg='black',
+    activeforeground='red', activebackground='grey',
+    command=draw_tutorial
+)
+tutorial_button.place(x=650, y=140)
+
+root.mainloop()
