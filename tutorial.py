03bb25b0cad4e0a49c3653bbc836cd1a82c9ebc4
SumarenkoED <Sumarenko.ED@phystech.edu>
2019-12-13 13:03:14 +0300

    small changes

--- tutorial.py
+++ tutorial.py
@@ -14,14 +14,13 @@ lbl1.grid(column=1, row=0)
 lbl2 = Label(window, text="1. Перед началом игры не забудьте\n переключиться на английскую раскладку и нажать TAB\n"
              "2. Комбинации клавиш:", font=("Arial Bold", 15), fg="#10455b")
 lbl2.grid(column=1, row=50)
-lbl3 = Label(window, text="'a','s','w','d' - передвижение\n"
-                          "'q','e' - поворот\n"
+lbl3 = Label(window, text="'A','S','W','D' - передвижение\n"
+                          "'E','E' - поворот\n"
                           "enter - стрелять\n"
-                          "'m' - показать карту",
+                          "H - использовать аптечку\n"
+                          "'M' - показать карту",
                           font=("Arial Bold", 13), bg="#00acb4", fg="#10455b", justify=LEFT)
 lbl3.grid(column=1, row=100)
-lbl4 = Label(window, text="ЧИТЫ:\n '+' - добавить hp\n '-' - убавить hp", font=("Arial Bold", 15), bg="red", justify=LEFT)
-lbl4.grid(column=1, row=150)
 btn = Button(window, text="Всё понятно", command=clicked, bg="#10455b", fg="#00acb4", font=("Arial Bold", 15))
 btn.grid(column=1, row=200)
 window.mainloop()
